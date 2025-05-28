37d2f16b9b06ebf4051170333122dbd662c07ddd
sawamuraya <sawamuraya_ss@yahoo.co.jp>
2025-05-28 19:43:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ import os
 import datetime
 
 # モデルファイルのパスとバージョン
-MODEL_PATH = "arch_classifier_model.keras"
+MODEL_PATH = "arch_classifier_model.h5"
 MODEL_VERSION = "arch_classifier_model_v3"
-GDRIVE_URL = "https://drive.google.com/uc?id=1FF2a19C4gTvYp3vFKpMJ1e-SdF5MB-rP"
+GDRIVE_URL = "https://drive.google.com/uc?id=1NmuLbyYsysLqTa49jSmKU2mRp7MzbJWS"
 MODEL_UPDATE_DATE = "2025/05/28"
 
 # モデルファイルがなければ Google Drive からダウンロード
@@ -79,4 +79,5 @@ if uploaded_file is not None:
 
     # インソール提案（仮）
     st.info(f"このタイプにおすすめのインソール：**インソール{pattern_id}番** をお試しください！")
+
 
